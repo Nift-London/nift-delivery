6d5e2cc1ea5a7924611bdf66d116273d1d748d63
Karol Gasienica-Fronek <karol.gasienica-fronek@lppsa.com>
2024-09-05 14:57:43 +0200

    NIFT-466

--- src/UI/Admin/DeliveryOrderCrudController.php
+++ src/UI/Admin/DeliveryOrderCrudController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 
 class DeliveryOrderCrudController extends AbstractCrudController
@@ -21,6 +22,7 @@ class DeliveryOrderCrudController extends AbstractCrudController
         return [
             IdField::new('id'),
             IdField::new('externalId'),
+            DateTimeField::new('createdAt'),
         ];
     }
 
