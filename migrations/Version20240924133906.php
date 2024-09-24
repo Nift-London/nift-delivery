e12b467d1a443893d80cc0bcd6fe49f6f8eb458b
Karol Gasienica-Fronek <karol.gasienica-fronek@lppsa.com>
2024-09-24 19:34:19 +0200

    NIFT-466

---
+++ migrations/Version20240924133906.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240924133906 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DELETE FROM quote');
+        $this->addSql('DELETE FROM delivery_order');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    }
+}
