f4c7aa3d5d3e6d2c0a57cd8bae1fc756d7fca372
Karol Gasienica-Fronek <karol.gasienica-fronek@lppsa.com>
2024-10-08 11:56:15 +0200

    Delivery multiple distances

--- src/UI/Admin/StoreCrudController.php
+++ src/UI/Admin/StoreCrudController.php
@@ -12,8 +12,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
